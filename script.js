407dda787c3106b3a429889807096fedfc665f40
Muhammad Tatma <muhammadtatma04@gmail.com>
2023-02-25 12:28:33 +0700

    remove unused code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const burger = document.querySelector(".hamburger");
 const links = document.querySelector(".links");
 const linksHref = document.querySelectorAll(".links li a")
-console.log("p");
 burger.addEventListener("click", ()=>{
     links.classList.toggle("show-links")
     
